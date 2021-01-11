a4c8bd8aebe6f2294415b1a9ff820d3ea7846c59
kayode <samsonkayode@gmail.com>
2021-01-11 02:06:46 +0100

    first commit

---
+++ src/main/java/com/backend/phoenixhealth/PhoenixHealthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.phoenixhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixHealthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixHealthBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/phoenixhealth/PhoenixHealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.phoenixhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixHealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
